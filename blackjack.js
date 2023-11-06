d757058ee3a3806ca8d3ecc66800a358acadc3dc
Mike Grilli <mikeg@downtownseattle.org>
2023-11-06 10:44:42 -0800

    updating assignment

--- blackjack.js
+++ blackjack.js
@@ -1,20 +1,80 @@
-// const blackjackDeck = getDeck();
+//import {getDeck} from './createCardDeck.js'
+//const blackjackDeck = getDeck();
 
+const blackjackDeck = () => {
+  const deck = []
+  const suits = ["Heart", "Diamond", "Clover", "Spade"]
+
+  suits.forEach(cardType => { 
+
+      for(let i = 1; i <= 13; i ++) {
+          
+          const card = {
+                    val : i,
+                    displayVal: i,
+                    suit: cardType
+                  }
+                  
+          if(card.displayVal === 1) {
+            card.displayVal = "Ace"
+          } 
+          else if( card.displayVal === 11) {
+            card.displayVal = "Jack"
+            card.val = 10
+          } 
+          else if( card.displayVal === 12) {
+            card.displayVal = "Queen"
+            card.val = 10
+          } 
+          else if( card.displayVal === 13) {
+            card.displayVal = "King"
+            card.val = 10
+          }
+
+          if(card.displayVal === "Ace") {
+              card.val = 11 
+
+          }
+
+      deck.push(card)
+
+      }
+  })
+  return deck
+}
+
+// console.log(blackjackDeck())
+//console.log(blackjackDeck())
 // /**
 //  * Represents a card player (including dealer).
 //  * @constructor
 //  * @param {string} name - The name of the player
 //  */
 class CardPlayer {
-  constructor(name, cards) {
-    this.name = name
-    this.cards = cards
+  constructor(player, hand) {
+    this.player = player
+    this.hand = []
+  }
+
+  drawCard() {
+      const randomNumber = Math.floor(Math.random() * 52)
+      const card = blackjackDeck()
+      this.hand.push(card[randomNumber])
   }
 }; //TODO
 
 // // CREATE TWO NEW CardPlayers
-const dealer = new CardPlayer("Nicole", []) // TODO
-const player = new CardPlayer("Nike", [])
+const dealer = new CardPlayer("Dealer"); // TODO
+const player = new CardPlayer("Player"); // TODO
+
+player.drawCard()
+player.drawCard()
+
+dealer.drawCard()
+dealer.drawCard()
+
+console.log(player)
+console.log(dealer)
 
 // /**
 //  * Calculates the score of a Blackjack hand
@@ -24,8 +84,32 @@ const player = new CardPlayer("Nike", [])
 //  * @returns {boolean} blackJackScore.isSoft
 //  */
 const calcPoints = (hand) => {
+  let isSoft = false
+  let blackJackScore = 0;
+  let aces = []
   
+  hand.forEach(item => {
+    blackJackScore += item.val
+    if (item.displayVal === "Ace") {
+      aces.push(item)
+    }
+  })
+
+  if (aces.length === 1 && blackJackScore < 21) {
+    isSoft = true 
+  } 
+
+  if (isSoft === true) {
+    blackJackScore = blackJackScore - 10
+  }
+
+  return {isSoft,blackJackScore}
+  // console.log({isSoft,blackJackScore})
+
 }
+
+
+
 
 // /**
 //  * Determines whether the dealer should draw another card.
@@ -33,10 +117,21 @@ const calcPoints = (hand) => {
 //  * @param {Array} dealerHand Array of card objects with val, displayVal, suit properties
 //  * @returns {boolean} whether dealer should draw another card
 //  */
-// const dealerShouldDraw = (dealerHand) => {
-//   // CREATE FUNCTION HERE
+const dealerShouldDraw = (dealerHand) => {
+  // CREATE FUNCTION HERE
+ 
+  const cards = calcPoints(dealerHand)
+  
+  if(cards.blackJackScore < 17) {
+    return true
+  } else {
+    return false
+  }
+  
 
-// }
+}
+
+dealerShouldDraw(dealer.hand)
 
 // /**
 //  * Determines the winner if both player and dealer stand
@@ -44,11 +139,11 @@ const calcPoints = (hand) => {
 //  * @param {number} dealerScore 
 //  * @returns {string} Shows the player's score, the dealer's score, and who wins
 //  */
-// const determineWinner = (playerScore, dealerScore) => {
-//   // CREATE FUNCTION HERE
+const determineWinner = (playerScore, dealerScore) => {
+  // CREATE FUNCTION HEREc
+  
 
-// }
-
+}
 // /**
 //  * Creates user prompt to ask if they'd like to draw a card
 //  * @param {number} count 
